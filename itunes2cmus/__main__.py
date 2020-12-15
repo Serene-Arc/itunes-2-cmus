b61127993be87039f6dc18e068e34ddf551b964c
Serene-Arc <serenical@gmail.com>
2020-12-15 15:17:04 +1000

    Use logger for output

--- itunes2cmus/__main__.py
+++ itunes2cmus/__main__.py
@@ -1,9 +1,14 @@
-import csv
+#!/usr/bin/env python3
+# coding=utf-8
+
 import argparse
+import csv
 import os
+import logging
 import pathlib
 
 parser = argparse.ArgumentParser(prog='itunes-2-cmus')
+logger = logging.getLogger()
 
 
 def initParser():
@@ -20,23 +25,29 @@ def initParser():
 if __name__ == '__main__':
     initParser()
     args = parser.parse_args()
-    print('Searching file...')
+    logger.info('Searching file...')
     songLocs = []
     with open(args.playlist, 'r', newline='', encoding='utf-16') as csvfile:
         rd = csv.DictReader(csvfile, delimiter='\t')
         for song in rd:
             songLocs.append(pathlib.PureWindowsPath(song['Location']))
 
-    print('{} songs found'.format(len(songLocs)))
+    logger.info('{} songs found'.format(len(songLocs)))
     songLocs = [str(path).strip(path.anchor) for path in songLocs]
 
     if args.mountfolder:
-        songLocs = [pathlib.PureWindowsPath(''.join(path.split(args.mountfolder)[1:])) for path in songLocs]
+        songLocs = [
+            pathlib.PureWindowsPath(''.join(path.split(args.mountfolder)[1:]))
+            for path in songLocs]
 
-    songLocs = [pathlib.PurePosixPath(pathlib.PurePosixPath(args.mount) / path.relative_to('\\')) for path in songLocs]
-    print('writing...')
+    songLocs = [
+        pathlib.PurePosixPath(
+            pathlib.PurePosixPath(args.mount) / path.relative_to('\\'))
+        for path in songLocs]
+
+    logger.info('writing...')
     with open(os.path.basename(args.playlist).split('.')[0] + '.pl', 'w', encoding='utf-8') as file:
         for song in songLocs:
             file.write(song.as_posix() + '\n')
 
-    print('Conversion Complete')
+    logger.info('Conversion Complete')
